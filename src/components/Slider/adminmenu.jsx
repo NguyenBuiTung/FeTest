2e56266fbba75887a77e03c9be6024b3991ba440
Tung Nguyen Bui <you@example.com>
2024-05-28 09:48:58 +0700

    ff

--- src/components/Slider/adminmenu.jsx
+++ src/components/Slider/adminmenu.jsx
@@ -1,13 +1,6 @@
 import React from "react";
 import {
-  GiftOutlined,
-  HistoryOutlined,
   HomeOutlined,
-  MessageOutlined,
-  SettingOutlined,
-  ShopOutlined,
-  UnlockOutlined,
-  UserOutlined,
 } from "@ant-design/icons";
 export const menuItems = [
   {
@@ -17,31 +10,7 @@ export const menuItems = [
     path: "/",
   },
   
-  {
-    key: "storemanager",
-    icon: <UserOutlined />,
-    label: "Post Manager",
-    path: "/post-manager",
-    
-  },
-  
-  
-  
-  {
-    key: "settings",
-    icon: <SettingOutlined />,
-    label: "Cài đặt",
-    path: "/settings",
-  },
- 
 ];
 export const rootSubmenuKeys = [
   "home",
-  "mcoupon",
-  "giaodich",
-  "storemanager",
-  "customer",
-  "settings",
-  "info-point-store",
-  "customer-see"
-];
+ ];
--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -4,48 +4,17 @@ import { persistReducer, persistStore } from "redux-persist";
 import thunk from "redux-thunk";
 import Login from "./login/Login";
 import Register from "./register/Register";
-import getListProgramCoupon from "./mcoupon/getListProgramCoupon";
-import addProgram from "./mcoupon/addProgram";
-import user from "./mcoupon/user"
-import storeManager from "./store/storeManager";
-import employeeManager from "./staff/employeeManager";
-import customer from "./customer/customer";
-import listAddress from "./customer/listAddress";
-import customerType from "./customer/customerType/customerType";
-import voucherRedux from "./voucher/voucherRedux";
-import sms from "./smsConfig/sms";
-import trade from "./trade/trade";
-import groupCustomer from "./customer/groupCustomer/groupCustomer";
-import customerSee from "./customerSee/customerSee";
 import dashboard from "./dashboard/dashboard";
-import rankCustomer from "./customer/rankCustomer/rankCustomer";
-import storeCustomer from "./customerSee/storeCustomer";
-
 
 const persistConfig = {
   key: "data",
   storage,
-//   whitelist: ["listProductBota"],
+  //   whitelist: ["listProductBota"],
 };
 export const rootReducers = combineReducers({
-    Login,
-    user,
-    Register,
-    getListProgramCoupon,
-    addProgram,
-    storeManager,
-    employeeManager,
-    customer,
-    listAddress,
-    customerType,
-    rankCustomer,
-    storeCustomer,
-    voucherRedux,
-    sms,
-    trade,
-    groupCustomer,
-    customerSee,
-    dashboard
+  Login,
+  Register,
+  dashboard,
 });
 const persistedReducer = persistReducer(persistConfig, rootReducers);
 export const store = configureStore({
